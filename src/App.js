77b7e5966514d794f52e301e5d249800a31e5acd
RohanSharma4792 <rohan.sharma2020@vitbhopal.ac.in>
2023-03-14 14:21:04 +0530

    https added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=c8134a93`;
+		const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=c8134a93`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
