37bb4de761d05b69922ef736cec73c3c31b26df2
RohanSharma4792 <rohan.sharma2020@vitbhopal.ac.in>
2023-03-15 20:07:34 +0530

    name changed and url also

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ const App = () => {
 	return (
 		<div className='container-fluid movie-app'>
 			<div className='row d-flex align-items-center mt-4 mb-4'>
-				<MovieListHeading heading='MoviePedia' />
+				<MovieListHeading heading='MoovyMaster' />
 				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
 			</div>
 			<div className='row'>
